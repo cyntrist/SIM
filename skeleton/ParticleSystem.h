bba723157ebb713873ae6fd45a4c60b228cd825a
cyntrist <cyntrist@ucm.es>
2024-10-18 10:28:53 +0200

    a

--- skeleton/ParticleSystem.h
+++ skeleton/ParticleSystem.h
@@ -1,25 +1,31 @@
 #pragma once
 
-#include <list>
+#include <random>
+#include <vector>
 #include "Particle.h"
 
 class ParticleSystem
 {
-	std::list<Particle> particles;
-	// origen (todo est oes el generator)
-	// velocidad media
-	//duracion
-	//particula modelo
-	// probabilidad
+	Particle* model = nullptr; // particula modelo
+	size_t max_num; // max numero de particulas
+	double min_life; // promedio vida
+	double max_life; 
+	std::vector<Particle*> particles; // vector de particulas
+	Vector3 origin;
+	Vector3 ini_speed;
+	Vector3 mean_speed;
+	double chance = 1; // probabilidad
+	std::uniform_real_distribution<> probability;
+	std::random_device rd{};
+	std::mt19937 mt{rd()};
 
 
 public:
-	ParticleSystem();
-	~ParticleSystem();
+	ParticleSystem() = default;
+	virtual ~ParticleSystem()  = default;
 
-	bool update(double t);
-	void addParticle();
-	void destroyParticle();
-
+	virtual bool update(double t);
+	virtual void addParticle();
+	virtual void destroyParticle();
 };
 
