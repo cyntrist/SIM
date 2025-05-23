0b2a90095806291164dc20b1452e14790c384c49
Cynthia Tristán <cyntrist@ucm.es>
2025-05-23 19:38:36 +0200

    Update RenderUtils.cpp

--- skeleton/RenderUtils.cpp
+++ skeleton/RenderUtils.cpp
@@ -1,7 +1,7 @@
 #include <vector>
 
 #include "PxPhysicsAPI.h"
-
+#include "iostream"
 #include "core.hpp"
 #include "RenderUtils.hpp"
 
@@ -9,9 +9,10 @@
 using namespace physx;
 
 extern void initPhysics(bool interactive);
-extern void stepPhysics(bool interactive, double t);	
+extern void stepPhysics(bool interactive, double t);
 extern void cleanupPhysics(bool interactive);
-extern void keyPress(unsigned char key, Camera* camera);
+extern void keyPress(unsigned char key, const PxTransform& camera);
+extern void keyReleased(unsigned char key, const PxTransform& camera);
 extern PxPhysics* gPhysics;
 extern PxMaterial* gMaterial;
 
@@ -45,109 +46,116 @@ double GetCounter()
 
 namespace
 {
-	Camera*	sCamera;
+	Camera* sCamera;
 
-void motionCallback(int x, int y)
-{
-	sCamera->handleMotion(x, y);
-}
+	void motionCallback(int x, int y)
+	{
+		sCamera->handleMotion(x, y);
+	}
 
-void keyboardCallback(unsigned char key, int x, int y)
-{
-	if(key==27)
-		exit(0);
+	void keyboardDownCallback(unsigned char key, int x, int y)
+	{
+		if (key == 27)
+			exit(0);
 
-	if(!sCamera->handleKey(key, x, y))
-		keyPress(key, sCamera);
-}
+		if (!sCamera->handleKey(key, x, y))
+			keyPress(key, sCamera->getTransform());
+	}
 
-void mouseCallback(int button, int state, int x, int y)
-{
-	sCamera->handleMouse(button, state, x, y);
-}
+	void keyboardUpCallback(unsigned char key, int x, int y)
+	{
+		if (!sCamera->handleKey(key, x, y))
+			keyReleased(key, sCamera->getTransform());
+	}
 
-void idleCallback()
-{
-	glutPostRedisplay();
-}
+	void mouseCallback(int button, int state, int x, int y)
+	{
+		sCamera->handleMouse(button, state, x, y);
+	}
 
-float stepTime = 0.0f;
-//#define FIXED_STEP
+	void idleCallback()
+	{
+		glutPostRedisplay();
+	}
 
-void renderCallback()
-{
-	double t = GetCounter();
+	float stepTime = 0.0f;
+	//#define FIXED_STEP
+
+	void renderCallback()
+	{
+		double t = GetCounter();
 #ifdef FIXED_STEP
-	if (t < (1.0f / 30.0f))
-	{
-		fprintf(stderr, "Time: %f\n", stepTime);
-		stepTime += t;
-	}
-	else
-		stepTime = 1.0f / 30.0f;
+		if (t < (1.0f / 30.0f))
+		{
+			fprintf(stderr, "Time: %f\n", stepTime);
+			stepTime += t;
+		}
+		else
+			stepTime = 1.0f / 30.0f;
 
-	if (stepTime >= (1.0f / 30.0f))
-	{
-		stepPhysics(true, stepTime);
-		stepTime = 0.0f;
-	}
+		if (stepTime >= (1.0f / 30.0f))
+		{
+			stepPhysics(true, stepTime);
+			stepTime = 0.0f;
+		}
 #else
-	stepPhysics(true, t);
+		stepPhysics(true, t);
 #endif
 
-	startRender(sCamera->getEye(), sCamera->getDir());
+		startRender(sCamera->getEye(), sCamera->getDir());
 
-	//fprintf(stderr, "Num Render Items: %d\n", static_cast<int>(gRenderItems.size()));
-	for (auto it = gRenderItems.begin(); it != gRenderItems.end(); ++it)
-	{
-		const RenderItem* obj = (*it);
-		auto objTransform = obj->transform;
-		if (!objTransform)
+		//fprintf(stderr, "Num Render Items: %d\n", static_cast<int>(gRenderItems.size()));
+		for (auto it = gRenderItems.begin(); it != gRenderItems.end(); ++it)
 		{
-			auto actor = obj->actor;
-			if (actor)
+			const RenderItem* obj = (*it);
+			auto objTransform = obj->transform;
+			if (!objTransform)
 			{
-				renderShape(*obj->shape, actor->getGlobalPose(), obj->color);
-				continue;
+				auto actor = obj->actor;
+				if (actor)
+				{
+					renderShape(*obj->shape, actor->getGlobalPose(), obj->color);
+					continue;
+				}
 			}
+			renderShape(*obj->shape, objTransform ? *objTransform : physx::PxTransform(PxIdentity), obj->color);
 		}
-		renderShape(*obj->shape, objTransform ? *objTransform : physx::PxTransform(PxIdentity), obj->color);
+
+		//PxScene* scene;
+		//PxGetPhysics().getScenes(&scene, 1);
+		//PxU32 nbActors = scene->getNbActors(PxActorTypeFlag::eRIGID_DYNAMIC | PxActorTypeFlag::eRIGID_STATIC);
+		//if (nbActors)
+		//{
+		//	std::vector<PxRigidActor*> actors(nbActors);
+		//	scene->getActors(PxActorTypeFlag::eRIGID_DYNAMIC | PxActorTypeFlag::eRIGID_STATIC, reinterpret_cast<PxActor**>(&actors[0]), nbActors);
+		//	renderActors(&actors[0], static_cast<PxU32>(actors.size()), true, Vector4(1.0f, 0.0f, 0.0f, 1.0f));
+		//}
+
+		finishRender();
 	}
 
-	//PxScene* scene;
-	//PxGetPhysics().getScenes(&scene, 1);
-	//PxU32 nbActors = scene->getNbActors(PxActorTypeFlag::eRIGID_DYNAMIC | PxActorTypeFlag::eRIGID_STATIC);
-	//if (nbActors)
-	//{
-	//	std::vector<PxRigidActor*> actors(nbActors);
-	//	scene->getActors(PxActorTypeFlag::eRIGID_DYNAMIC | PxActorTypeFlag::eRIGID_STATIC, reinterpret_cast<PxActor**>(&actors[0]), nbActors);
-	//	renderActors(&actors[0], static_cast<PxU32>(actors.size()), true, Vector4(1.0f, 0.0f, 0.0f, 1.0f));
-	//}
-
-	finishRender();
-}
-
-void exitCallback(void)
-{
-	delete sCamera;
-	cleanupPhysics(true);
-}
+	void exitCallback(void)
+	{
+		delete sCamera;
+		cleanupPhysics(true);
+	}
 }
 
 void renderLoop()
 {
 	StartCounter();
-	sCamera = new Camera(PxVec3(50.0f, 50.0f, 50.0f), PxVec3(-0.6f,-0.2f,-0.7f));
+	sCamera = new Camera(PxVec3(50.0f, 50.0f, 50.0f), PxVec3(-0.6f, -0.2f, -0.7f));
 
-	setupDefaultWindow("Simulacion Fisica Videojuegos");
+	setupDefaultWindow("Simulacion Fisica Videojuegos : SPACE EXPLORER SIMULATOR");
 	setupDefaultRenderState();
 
 	glutIdleFunc(idleCallback);
 	glutDisplayFunc(renderCallback);
-	glutKeyboardFunc(keyboardCallback);
+	glutKeyboardFunc(keyboardDownCallback);
+	glutKeyboardUpFunc(keyboardUpCallback); // callback para cuando dejas de pulsar una tecla
 	glutMouseFunc(mouseCallback);
 	glutMotionFunc(motionCallback);
-	motionCallback(0,0);
+	motionCallback(0, 0);
 
 	atexit(exitCallback);
 
@@ -162,6 +170,8 @@ void RegisterRenderItem(const RenderItem* _item)
 
 void DeregisterRenderItem(const RenderItem* _item)
 {
+
+	std::cout << "Desregistrando renderItem..." << std::endl;
 	auto it = find(gRenderItems.begin(), gRenderItems.end(), _item);
 	if (it == gRenderItems.end()) return;
 	gRenderItems.erase(it);
